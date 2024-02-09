22f799742b56d34c97703d70ad001ac6163cd343
Duotech B13 <duotech@gmail.com>
2024-02-08 19:48:37 -0500

    first

---
+++ Engineer.java
@@ -0,0 +1,30 @@
+package myPack;
+
+public class Engineer {
+
+// fields
+    private String name;
+    private String position;
+
+    public Engineer(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+    
+// getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
